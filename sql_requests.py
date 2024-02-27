b6d46546e30c01920e4f14c3506d718c17fdd20b
FlorianK02 <147732845+FlorianK02@users.noreply.github.com>
2024-02-27 19:42:10 +0100

    Update sql_requests.py

--- sql_requests.py
+++ sql_requests.py
@@ -1,14 +1,15 @@
+import azure.functions as func
+import logging
+import sqlalchemy
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+
 def getScore(username):
     engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
     with engine.connect() as connection:
     result = connection.execute("SELECT score FROM [dbo].[Users] WHERE username = '"+username+"';")
-    rows = result.fetchall()
-    return rows
-
-def getPW(username):
-    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-    with engine.connect() as connection:
-    result = connection.execute("SELECT password FROM [dbo].[Users] WHERE username = '"+username+"';")
     rows = result.fetchall()
     return rows
 
@@ -42,3 +43,26 @@ def getDashboard():
     result = connection.execute("SELECT username, score FROM [dbo].[Users];")
     rows = result.fetchall()
     return rows
+
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    score = getScore("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             score
+             status_code=200
+        )
