ebc9bb054bd207da37698b6acad9b6c4e9910c39
FlorianK02 <147732845+FlorianK02@users.noreply.github.com>
2024-02-28 10:45:47 +0100

    Update sql_requests.py

--- sql_requests.py
+++ sql_requests.py
@@ -4,31 +4,31 @@ import sqlalchemy
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver???")
 
 def getScore(username):
-    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver???")
     with engine.connect() as connection:
     result = connection.execute("SELECT score FROM [dbo].[Users] WHERE username = '"+username+"';")
     rows = result.fetchall()
     return rows
 
 def pushScore(username, newScore):
-    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver???")
     with engine.connect() as connection:
     result = connection.execute("UPDATE [dbo].[Users] SET score = "+newScore+" WHERE username = '"+username+"';")
     rows = result.fetchall()
     return rows
 
 def register(username, password):
-    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver???")
     with engine.connect() as connection:
     result = connection.execute("INSERT INTO [dbo].[Users] (username, password, score) VALUES ('"+username+"', '"+password+"', 0);")
     rows = result.fetchall()
     return rows
 
 def verifypw(username, password):
-    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver???")
     with engine.connect() as connection:
     result = connection.execute("SELECT password FROM [dbo].[Users] WHERE username = '"+username+"';")
     rows = result.fetchall()
@@ -38,7 +38,7 @@ def verifypw(username, password):
         return False
 
 def getDashboard():
-    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver={ODBC Driver 18 for SQL Server};Server=tcp:boschhackathon.database.windows.net,1433;Database=HyperMilingDB;Uid=bosch-hackathon;Pwd={aEVcmVBt2mfvRLZKh3};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    engine = sqlalchemy.create_engine("mssql+pyodbc:///?odbc_connect=Driver???")
     with engine.connect() as connection:
     result = connection.execute("SELECT username, score FROM [dbo].[Users];")
     rows = result.fetchall()
